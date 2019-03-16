a30a7827817450d745bea79b846c771596354ffd
Vlad Morosanu <vladmorosanu97@gmail.com>
2019-03-16 12:23:17 +0200

    Updates

---
+++ precache-manifest.3ef302d1f7f7f73a05c7370555696bc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7151c3776d485bd7c440",
+    "url": "/react-todone/static/js/runtime~main.c454c413.js"
+  },
+  {
+    "revision": "1f7b5a998e5cc41a1cd6",
+    "url": "/react-todone/static/js/main.7b9143c5.chunk.js"
+  },
+  {
+    "revision": "bb7e5c677e95166b1715",
+    "url": "/react-todone/static/js/2.b6b1eb87.chunk.js"
+  },
+  {
+    "revision": "1f7b5a998e5cc41a1cd6",
+    "url": "/react-todone/static/css/main.5acea928.chunk.css"
+  },
+  {
+    "revision": "cbfb9d3cb788a7cc6541bd04554e1ea6",
+    "url": "/react-todone/index.html"
+  }
+];
